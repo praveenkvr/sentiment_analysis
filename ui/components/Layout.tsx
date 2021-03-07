7dd1df93cb103935828b8c04c7356d18f9f5c9ae
Praveen <venkata.kuruvada@gmail.com>
2021-03-07 15:31:01 -0800

    Fix meta

--- ui/components/Layout.tsx
+++ ui/components/Layout.tsx
@@ -11,6 +11,7 @@ const Layout = ({ children, title = 'Sentiment Analysis' }: Props) => (
   <div className={css.container}>
     <Head>
       <title>{title}</title>
+      <meta name="description" content="Analyze tweets sentiment based on search" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />      <meta charSet="utf-8" />
